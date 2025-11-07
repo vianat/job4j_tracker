e6a1db052afd7b1499fd56a794173acc3407c2fd
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-11-07 12:05:21 -0600

    4

---
+++ src/main/java/inheritance/JSONReport.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class JSONReport extends TextReport {
+
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator()
+                + "\t\"name\"" + " : \"" + name + "\"," + System.lineSeparator()
+                + "\t\"body\"" + " : \"" + body + "\"" + System.lineSeparator()
+                + "}";
+    }
+}
