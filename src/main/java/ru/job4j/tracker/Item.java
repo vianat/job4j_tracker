d2414cc08a01391b8e35de0bb992e98ff92f4da0
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-11-07 13:34:59 -0600

    8

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,10 +1,19 @@
 package ru.job4j.tracker;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
+    }
+
+    public void getCreated() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = created.format(formatter);
+        System.out.println("Formatted date & time: " + currentDateTimeFormat);
     }
 
     public Item(String name) {
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,8 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item one = new Item();
+        one.getCreated();
+    }
+}
