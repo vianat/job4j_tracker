126ea712b10381236db1b681a13378d7621571c6
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-11-08 11:46:29 -0600

    3.2

---
+++ src/main/java/encapsulation/ShopDrop.java
@@ -0,0 +1,19 @@
+package encapsulation;
+
+public class ShopDrop {
+    public static Product[] delete(Product[] products, int index) {
+        if (index == products.length - 1) {
+            products[products.length - 1] = null;
+            return products;
+        }
+        products[index] = products[index + 1];
+        for (int i = index++; i < products.length ; i++) {
+            if(i == products.length - 1){
+                break;
+            }
+            products[i] = products[i + 1];
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+}
