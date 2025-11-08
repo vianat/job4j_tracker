c50cb1bd1d006eb2c7cd5c9e6ffa7943fa940406
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-11-08 12:01:04 -0600

    4fix

--- src/main/java/encapsulation/License.java
+++ src/main/java/encapsulation/License.java
@@ -11,7 +11,9 @@ public class License {
 
     @Override
     public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         License license = (License) o;
         return Objects.equals(owner, license.owner) && Objects.equals(model, license.model) && Objects.equals(code, license.code) && Objects.equals(created, license.created);
     }
