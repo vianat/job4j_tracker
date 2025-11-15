999d3677fb2b8aec9e760e79532e78666f16a79f
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-11-15 10:51:48 -0600

    7delete item

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -51,10 +51,19 @@ public class Tracker {
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
         if (index != -1) {
-            item.setId(id);        // сохраняем старый id
+            item.setId(id);        // save old id
             items[index] = item;
             return true;
         }
         return false;
+    }
+
+    public void delete(int id) {
+        if (id <= size) {
+            int index = indexOf(id);
+            System.arraycopy(items, index, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
     }
 }
