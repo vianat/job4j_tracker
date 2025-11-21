55c8b5ccdd70902cd3e9a18a330fcc9a4295ea8a
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-11-21 17:02:54 -0600

    fix formatter

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -6,6 +6,7 @@ public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
     }
@@ -45,12 +46,11 @@ public class Item {
 
     @Override
     public String toString() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
         return "Item{"
                 + "id=" + id
                 + ", name='" + name + '\''
-                + ", created=" + created.format(formatter)
+                + ", created=" + created.format(FORMATTER)
                 + '}';
     }
 }
