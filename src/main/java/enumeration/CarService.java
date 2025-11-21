ea2a9414a53db85a8a22db7e72d5ad9021b38eb6
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-11-21 16:56:16 -0600

    enums

---
+++ src/main/java/enumeration/CarService.java
@@ -0,0 +1,12 @@
+package enumeration;
+
+public class CarService {
+
+    public static void main(String[] args) {
+        Status toyota = Status.FINISHED;
+        Status volvo = Status.WAITING;
+        System.out.println("Статус Toyota: " + toyota);
+        System.out.println("Статус Volvo: " + volvo);
+    }
+
+}
