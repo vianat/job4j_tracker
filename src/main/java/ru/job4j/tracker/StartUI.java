67d5df3811389795c0a4b4f96f5c34cc3b060335
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-12-13 12:14:34 -0600

    3.0

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,15 +1,37 @@
 package ru.job4j.tracker;
-import java.time.format.DateTimeFormatter;
+
+import java.util.Scanner;
 
 public class StartUI {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Выбрать: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        String[] menu = {
+                "Добавить новую заявку", "Показать все заявки", "Изменить заявку",
+                "Удалить заявку", "Показать заявку по id", "Показать заявки по имени",
+                "Завершить программу"
+        };
+        for(int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
 
     public static void main(String[] args) {
-        Item one = new Item();
-        Item two = new Item(222, "two");
-
-        String currentDateTimeFormat = one.getCreated().format(FORMATTER);
-        System.out.println("Formatted date & time: " + currentDateTimeFormat);
-        System.out.println(two);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
