0d9c1d129dec5961a658d8949eb6b32bb83d4ba1
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-12-13 12:17:01 -0600

    3.0 checkstyle

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -24,7 +24,7 @@ public class StartUI {
                 "Удалить заявку", "Показать заявку по id", "Показать заявки по имени",
                 "Завершить программу"
         };
-        for(int i = 0; i < menu.length; i++) {
+        for (int i = 0; i < menu.length; i++) {
             System.out.println(i + ". " + menu[i]);
         }
     }
