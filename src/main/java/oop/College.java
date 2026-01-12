b0232092e02ad3234eb4419bc6b3ade76cef1926
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2026-01-12 11:04:40 -0600

    5 casting

---
+++ src/main/java/oop/College.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        Freshman freshman = student;
+        Object obj = student;
+    }
+}
---
+++ src/main/java/oop/Student.java
@@ -0,0 +1,4 @@
+package oop;
+
+public class Student extends Freshman {
+}
