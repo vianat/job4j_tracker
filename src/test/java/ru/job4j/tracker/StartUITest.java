4f6153591879ae3308163ef97c83d9d5afbeecf0
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2026-01-12 12:33:58 -0600

    8 test for create item

---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StartUITest {
+    @Test
+    void whenAddItem() {
+        String[] answers = {"0"};
+        Input input = new MockInput(answers);
+        Tracker tracker = new Tracker();
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("0");
+        assertThat(created.getName()).isEqualTo(expected.getName());
+    }
+}
