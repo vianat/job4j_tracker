d60bc67c2b9b2f549b269afbe7d406d5601a3f4e
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2026-01-13 14:11:40 -0600

    11 strategy

---
+++ src/main/java/strategy/Square.java
@@ -0,0 +1,11 @@
+package strategy;
+
+public class Square implements Shape {
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return    " |---|" + ln
+                + " |   |" + ln
+                + " |___|" + ln;
+    }
+}
